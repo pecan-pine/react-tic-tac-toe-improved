d7a4809d002d9949731d32c0b158017abe8a65f9
pecan.pine <pecan.pine@gmail.com>
2020-09-24 12:39:27 -0700

    Updates

---
+++ precache-manifest.59709a85e9326884eef6ab6c0c0f0846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eade3041d7aeff275de2e543ee5e5469",
+    "url": "/react-tic-tac-toe-improved/index.html"
+  },
+  {
+    "revision": "ef164e8cb7913588b101",
+    "url": "/react-tic-tac-toe-improved/static/css/main.65e7ceed.chunk.css"
+  },
+  {
+    "revision": "e7145cf0435c65dc9cea",
+    "url": "/react-tic-tac-toe-improved/static/js/2.5d732802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe-improved/static/js/2.5d732802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef164e8cb7913588b101",
+    "url": "/react-tic-tac-toe-improved/static/js/main.1fa966d4.chunk.js"
+  },
+  {
+    "revision": "58270990ddd6e901abe6",
+    "url": "/react-tic-tac-toe-improved/static/js/runtime-main.2f9c88e6.js"
+  },
+  {
+    "revision": "44d3346685f5e816e95c2a842496807e",
+    "url": "/react-tic-tac-toe-improved/static/media/stars.44d33466.jpg"
+  }
+]);
